6492b7440f357d38bb4510d09da96674aa110eee
sampo.kuisma <sampo.kuisma@accenture.com>
2018-12-22 00:36:13 +0200

    Day 9 cleared. Changed ArrayList -> LinkedList

--- src/aoc2018/Day09.java
+++ src/aoc2018/Day09.java
@@ -1,9 +1,6 @@
 package aoc2018;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,71 +9,67 @@ public class Day09 {
     public static void main(String[] args) {
         Long curTime = System.nanoTime();
         Day09 day = new Day09();
-        day.solve();
+        day.solve(1);
+        day.solve(100);
         Long delta = System.nanoTime() - curTime;
         System.out.println("Elapsed time: " + delta / 1000000 + "ms");
     }
 
-    private void solve() {
+    private void solve(int multiplier) {
         String dataPath = "src\\resources\\input-day9-1.txt";
         FileReader fr = new FileReader(dataPath);
         String[] data = fr.readFirstRow().replaceAll("[a-z ]", "").split(";");
 
         int players = Integer.parseInt(data[0]);
         int currentPlayer = 0;
-        List<Integer> scores = new ArrayList<>();
+        List<Long> scores = new ArrayList<>();
         for (int i = 0; i < players; i++) {
-            scores.add(i, 0);
+            scores.add(i, 0L);
         }
-        int maxMarble = Integer.parseInt(data[1]);
-        int marble = 1;
-        //List<Integer> game = new ArrayList<>();
+        int maxMarble = Integer.parseInt(data[1])*multiplier;
+
         List<Integer> game = new LinkedList<>();
+        ListIterator<Integer> currentMarble = game.listIterator();
         // Initialize game
-        game.add(0);
-        game.add(1, 1);
-        game.add(1, 2);
-        int currentMarble = 2;
+        currentMarble.add(0);
+        currentMarble.add(1);
+        currentMarble.previous();
+        currentMarble.add(2);
 
         for (int i = 3; i <= maxMarble; i++) {
             if ( i % 23 == 0) { // Points round
-                int roundScore = scores.get(currentPlayer) + i;
+                Long roundScore = scores.get(currentPlayer) + i;
+                int value = 0;
 
-                int index = getNextIndex(game, currentMarble, 7, false);
-                roundScore += game.remove(index);
-                currentMarble = game.get(index);
-
-                scores.set(currentPlayer, roundScore);
-                //System.out.println(i);
+                for (int j = 0; j < 8; j++) {
+                    if (currentMarble.hasPrevious()) {
+                        value = currentMarble.previous();
+                    } else {
+                        currentMarble = game.listIterator(game.size());
+                        value = currentMarble.previous();
+                    }
+                }
+                currentMarble.remove();
+                currentMarble.next();
+                scores.set(currentPlayer, roundScore + value);
             } else { // Normal round
-                int place = getNextIndex(game, currentMarble, 2, true);
-                game.add(place, i);
-                currentMarble = i;
+                if (currentMarble.hasNext()) {
+                    currentMarble.next();
+                } else {
+                    currentMarble = game.listIterator(0);
+                    currentMarble.next();
+                }
+                currentMarble.add(i);
             }
 
-            //System.out.println(currentPlayer + " " + currentMarble + " " + game);
+            //System.out.println(currentPlayer + " " + game);
             currentPlayer = changeTurn(currentPlayer, players);
         }
-        //scores.stream().forEach(System.out::println);
-        System.out.println(scores.stream().mapToInt(Integer::intValue).max().getAsInt());
+        System.out.println(scores.stream().mapToLong(Long::longValue).max().getAsLong());
     }
-
-    private int getNextIndex(List<Integer> game, int currentMarble, int move, boolean clockwise){
-        int index = 0;
-        if (clockwise) {
-            int nextIndex = game.indexOf(currentMarble) + move;
-            index = game.size() >= nextIndex ? nextIndex : nextIndex - game.size();
-        } else {
-            int nextIndex = game.indexOf(currentMarble) - move;
-            index = nextIndex > 0 ? nextIndex : game.size() + nextIndex;
-        }
-        return index;
-    }
-
 
     private int changeTurn(int t, int max) {
         t++;
         return t == max ? 0 : t;
     }
 }
-
